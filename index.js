0c27907611b2b0c9dc78672db2c7b63c522ac77f
cristianaquino <ycristian10@gmail.com>
2022-09-22 23:35:46 -0500

    index

--- index.js
+++ index.js
@@ -3,55 +3,22 @@ require("./mongo");
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const Note = require("./models/Note");
 const handleErrors = require("./middleware/handleErrors");
+const usersRouter = require("./controllers/users");
+const notesRouter = require("./controllers/notes");
+const loginRouter = require("./controllers/login");
 
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/api/notes", (req, res) => {
-  Note.find({}).then((note) => {
-    res.send(note);
-  });
-});
-
-app.post("/api/notes", (req, res) => {
-  const body = req.body;
-  const newNote = new Note({ ...body, date: new Date() });
-  newNote.save().then((savedNote) => res.send(savedNote));
-});
-
-app.get("/api/notes/:id", (req, res, next) => {
-  const { id } = req.params;
-  Note.findById(id)
-    .then((note) => {
-      if (note) {
-        res.send(note);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-app.put("/api/notes/:id", (req, res, next) => {
-  const { id } = req.params;
-  const body = req.body;
-  Note.findByIdAndUpdate(id, body, { new: true })
-    .then((note) => res.send(note))
-    .catch((error) => next(error));
-});
-
-app.delete("/api/notes/:id", (req, res, next) => {
-  const { id } = req.params;
-  Note.findByIdAndDelete(id)
-    .then(() => res.status(204).end())
-    .catch((error) => next(error));
-});
-
+app.use("/api/users", usersRouter);
+app.use("/api/notes", notesRouter);
+app.use("/api/login", loginRouter);
 // middleware
 app.use(handleErrors);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log("http://localhost:3000/");
 });
+
+module.exports = { app, server };
